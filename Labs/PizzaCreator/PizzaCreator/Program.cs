969fb165d04c1f2e9f6ed33df0c6cd7a341fafa8
David Rojo <david.rojo@my.tccd.edu>
2019-01-29 22:33:45 -0600

    commit

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -11,6 +11,59 @@ namespace PizzaCreator
         static void Main( string[] args )
         {
             Console.WriteLine("Hello Pizza Creator");
+            DisplayMenu();
+        }
+
+        private static void DisplayMenu()
+        {
+            //new order
+            var neworder = "New Order";
+            //modify order
+            var modifyorder = "Modify Order";
+            //display order
+            var displayorder = "Display Order";
+            //quit
+            var quit = "Quit";
+
+            while (true)
+            {
+                Console.WriteLine("Please choose one of the following: ");
+                Console.WriteLine("1. " + neworder);
+                Console.WriteLine("2. " + modifyorder);
+                Console.WriteLine("3. " + displayorder);
+                Console.WriteLine("4. " + quit);
+
+                string choice;
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                    NewOrder();
+                else if (choice == "2")
+                    ModifyOrder();
+                else if (choice == "3")
+                    DisplayOrder();
+                else if (choice == "4")
+                    break;
+                else
+                {
+                    Console.WriteLine("Please enter a valid option: ");
+                    DisplayMenu();
+                }
+
+            }
+        }
+
+        private static void NewOrder()
+        {
+            Console.WriteLine("This is the New Order. ");
+        }
+        private static void ModifyOrder()
+        {
+            Console.WriteLine("This is the Modified order. ");
+        }
+        private static void DisplayOrder()
+        {
+            Console.WriteLine("This is your order. ");
         }
     }
 }
