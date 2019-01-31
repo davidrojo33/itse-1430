ca5b7d55f086d8771152b5f2668627b20497e7d6
David Rojo <david.rojo@my.tccd.edu>
2019-01-30 21:13:12 -0600

    commit

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -105,8 +105,8 @@ namespace HelloWorld
             10.ToString(); // "10"
 
             //Is string empty?
-            string input = null;
-            int length = input.Length;
+            string input = "";
+            //int length = input.Length;
             bool isEmpty;
 
             // 1.
@@ -198,6 +198,31 @@ namespace HelloWorld
                     return result;
 
                 Console.WriteLine("Enter a valid decimal value");
+            };
+        }
+
+        private static void PlayWithArrays()
+        {
+            //int size = 100;
+            int[] prices = new int[100];
+            for (var index = 0; index < prices.Length; ++index)
+            {
+                prices[index] = index + 1;
+            };
+
+            DisplayArray(prices);
+
+            var input = "field1,field2,field3;field4,,field5";
+            var fields = input.Split(',', ';');
+        }
+
+        private static void DisplayArray( int[] values/*, int count*/ )
+        {
+            //for (var index = 0; index < values.Length; ++index)
+            foreach (var item in values)
+            {
+                //Console.WriteLine(values[index]);
+                Console.WriteLine(item);
             };
         }
 
