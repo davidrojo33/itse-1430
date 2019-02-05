be21067a2754c9e9ef8ca8332a4e8b6793c0cb53
David Rojo <david.rojo@my.tccd.edu>
2019-02-04 21:36:10 -0600

    Commit

---
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameManager.Host.Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadUI();
+        }
+
+        void LoadUI ()
+        {
+            Game game = new Game();
+
+            
+            game.Name = "DOOM";
+            game.Price = 59.99m;
+        }
+    }
+}
