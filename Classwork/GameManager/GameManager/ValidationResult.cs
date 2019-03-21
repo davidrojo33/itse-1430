944f45bf090fcecf1d3d65ed726d6b70e253313b
David Rojo <david.rojo@my.tccd.edu>
2019-03-20 21:40:17 -0500

    cmiit

--- Classwork/GameManager/GameManager/ValidationResult.cs
+++ Classwork/GameManager/GameManager/ValidationResult.cs
@@ -2,5 +2,13 @@
 {
     public class ValidationResult
     {
+        private string v1;
+        private string[] v2;
+
+        public ValidationResult( string v1, string[] v2 )
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
     }
 }
