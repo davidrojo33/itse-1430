c5e2102f3ef3573a7487b6882ebdc96fb4781497
David Rojo <david.rojo@my.tccd.edu>
2019-03-31 17:04:15 -0500

    Commit

---
+++ Labs/ContactManager.UI/ContactManager/ContactDatabase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    public class ContactDatabase : IContactDatabase
+    {
+        public Contacts Add( Contacts contact )
+        {
+            if (contact == null)
+            throw new ArgumentException(nameof(contact));
+
+            ObjectValidator.Validate(contact);
+
+            var existing = FindByName(contact.Name);
+            if (existing != null)
+                throw new Exception("Contact must be unique.");
+
+            return AddCore(contact);
+        }
+
+        public void Delete( int id )
+        {
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0.");
+
+            DeleteCore(id);
+        }
+
+        public Contacts Get( int id )
+        {
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0");
+
+            return GetCore(id);
+        }
+
+        public IEnumerable<Contacts> GetAll()
+        {
+            return GetAllCore();
+        }
+
+        public Contacts Update( int id, Contacts contact )
+        {
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0.");
+            if (contact == null)
+                throw new ArgumentNullException(nameof(contact));
+
+            ObjectValidator.Validate(contact);
+
+            var existing = GetCore(id);
+            if (existing != null)
+                throw new Exception("Contact does not exit.");
+
+            var sameName = FindByName(contact.Name);
+            if (sameName != null && sameName.Id != id)
+                throw new Exception("Contact must be unique.");
+
+            return UpdateCore(id, contact);
+        }
+
+        protected abstract Contacts AddCore( Contacts contact );
+
+        protected abstract void DeleteCore( int id );
+
+        protected virtual Contacts FindByName(string name)
+        {
+            foreach ( var contact in GetAllCore())
+            {
+                if (String.Compare(contact.Name, name, true) == 0)
+                    return contact;
+            };
+            return null;
+        }
+
+        protected abstract Contacts GetCore( int id );
+
+        protected abstract IEnumerable<Contacts> GetAllCore();
+
+        protected abstract Contacts UpdateCore( int id, Contacts newContact );
+    }
+}
---
+++ Labs/ContactManager.UI/ContactManager/MemoryContactDatabase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    class MemoryContactDatabase : ContactDatabase
+    {
+
+        protected override Contacts AddCore( Contacts contact )
+        {
+            contact.Id = ++_nextId;
+            _items.Add(Clone(contact));
+
+            return contact;
+        }
+
+        protected override void DeleteCore( int id )
+        {
+            var index = GetIndex(id);
+            if (index >= 0)
+                _items.RemoveAt(index);
+        }
+
+        protected override Contacts GetCore( int id )
+        {
+            var index = GetIndex(id);
+            if (index >= 0)
+                return Clone(_items[index]);
+
+            return null;
+        }
+
+        protected override IEnumerable<Contacts> GetAllCore()
+        {
+            foreach (var item in _items)
+                yield return Clone(item);
+        }
+
+        protected override Contacts UpdateCore( int id, Contacts contact )
+        {
+            var index = GetIndex(id);
+
+            contact.Id = id;
+            var existing = _items[index];
+            Clone(existing, contact);
+
+            return contact;
+        }
+
+        private Contacts Clone(Contacts contact)
+        {
+            var newContact = new Contacts();
+            Clone(newContact, contact);
+
+            return newContact;
+        }
+
+        private void Clone(Contacts target, Contacts source)
+        {
+            target.Id = source.Id;
+            target.Name = source.Name;
+            target.Email = source.Email;
+        }
+
+        private int GetIndex(int id)
+        {
+            for (var index = 0; index < _items.Count; ++index)
+                return index;
+
+            return -1;
+        }
+    }
+}
