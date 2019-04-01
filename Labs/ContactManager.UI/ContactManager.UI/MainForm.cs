3c2b823ec6f41c8f69ab3c70ac018338b4a6416e
David Rojo <david.rojo@my.tccd.edu>
2019-03-31 22:51:01 -0500

    Commit

--- Labs/ContactManager.UI/ContactManager.UI/MainForm.cs
+++ Labs/ContactManager.UI/ContactManager.UI/MainForm.cs
@@ -16,5 +16,70 @@ namespace ContactManager.UI
         {
             InitializeComponent();
         }
+
+        private void OnFileExit( object sender, EventArgs e)
+        {
+            var x = 10;
+
+            Close();
+        }
+
+        protected override void OnLoad (EventArgs e )
+        {
+            base.OnLoad(e);
+
+            BindingList();
+        }
+
+        private void OnHelpAbout ( object sender, EventArgs e )
+        {
+            var form = new AboutBox();
+            form.ShowDialog();
+        }
+
+        private void BindList()
+        {
+            listContacts.Items.Clear();
+            listContacts.DisplayMember = nameof(Contact.Name);
+            listContacts.Items.AddRange(Contacts.GetAll());
+        }
+
+        private void OnContactAdd ( object sender, EventArgs e)
+        {
+            var form = new ContactForm();
+
+            while (true)
+            {
+                if (form.ShowDialog(this) != DialogResult.OK)
+                    return;
+
+                    try 
+                    {
+                        OnSafeAdd(form);
+                        break;
+                    } catch (InvalidOperationException)
+                    {
+                        MessageBox.Show(this, "Choose a better contact." , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    } catch (Exception ex)
+                    {
+                        DisplayError(ex);
+                    };
+            };
+
+            BindList();
+        }
+
+        private void DisplayError(Exception ex)
+        {
+            MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void OnSafeAdd( ContactForm form)
+        {
+            try
+            {
+                Contacts.
+            }
+        }
     }
 }
