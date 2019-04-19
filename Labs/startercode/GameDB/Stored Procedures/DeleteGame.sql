87b452136021f2aa8a2cd205bb2f73b3c96a56ed
David Rojo <david.rojo@my.tccd.edu>
2019-04-19 15:10:32 -0500

    commit

---
+++ Labs/startercode/GameDB/Stored Procedures/DeleteGame.sql
@@ -0,0 +1,15 @@
+﻿--
+-- Deletes a game.
+--
+-- PARAMS:
+--    id - The ID of the game to remove.
+--
+-- RETURNS: None.
+--
+CREATE PROCEDURE [dbo].[DeleteGame]
+	@id INT
+AS BEGIN
+	SET NOCOUNT ON;
+
+    DELETE FROM Games WHERE Id = @id
+END
---
+++ Labs/startercode/GameDB/Stored Procedures/FindByName.sql
@@ -0,0 +1,19 @@
+﻿--
+-- Searches for a game given its name.
+--
+-- PARAMS:
+--    name - The name to search for.
+--
+-- RETURNS: The game, if found.
+--
+CREATE PROCEDURE [dbo].[FindByName]
+	@name NVARCHAR(255)
+AS BEGIN
+    SET NOCOUNT ON;
+
+    SET @name = LTRIM(RTRIM(ISNULL(@name, '')))
+
+    SELECT Id, Name, Description, Price, Owned, Completed
+    FROM Games
+    WHERE Name = @name
+END
